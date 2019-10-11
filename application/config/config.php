b35eccd1b77ec9e8ae4e11209990b4eb06e2742c
Dionisio Febrianto <dio.721999@gmail.com>
2019-10-11 14:04:21 +0700

    update config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/bisaNikah.com/BisaNikah.com/';
+$config['base_url'] = 'http://localhost/BisaNikah.com/';
 
 /*
 |--------------------------------------------------------------------------
