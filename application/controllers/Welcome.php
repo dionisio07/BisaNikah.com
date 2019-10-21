1e72ef1f2c81fc56247374382073b9c46b4efd8b
Dionisio Febrianto <dio.721999@gmail.com>
2019-10-21 16:29:53 +0700

    view

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,7 +20,7 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('header');
-		$this->load->view('homeFix');
+		//$this->load->view('header');
+		$this->load->view('home');
 	}
 }
